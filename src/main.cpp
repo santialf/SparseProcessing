b8b22e59b34eb6374def53f824ab7e04fcd22743
shanti <santiago.quintas@hotmail.com>
2026-01-02 19:17:16 +0000

    Fix compilation of mmio library

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-#include "mmio.c"
-#include "smsh.c"
 
+extern "C" {
+#include "mmio.h"
+}
 
 int main(int argc, char* argv[]) {
 
     int ret_code;
     MM_typecode matcode;
     FILE *f;
-    int A_num_rows, A_num_cols, nz, A_nnz;
-    int i = 0, *I_complete, *J_complete;
-    float *V_complete;
+    int A_num_rows, A_num_cols, nz;
+    int *I_complete, *J_complete;
 
-    std::cout << "Hello World!" << std::endl;
-    for(int i = 0; i < argc; i++) {
-        std::cout << "arg[" << i << "]: " << argv[i] << std::endl;
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " matrix.zip\n";
+        return 1;
     }
 
-    /*******************************************************************/
     if ((f = fopen(argv[1], "r")) == NULL)
     {
         printf("Could not locate the matrix file. Please make sure the pathname is valid.\n");
@@ -35,98 +34,6 @@ int main(int argc, char* argv[]) {
         printf("Could not read matrix dimensions.\n");
         exit(1);
     }
-    
-    if ((strcmp(matcode, "MCRG") == 0) || (strcmp(matcode, "MCIG") == 0) || (strcmp(matcode, "MCPG") == 0) || (strcmp(matcode, "MCCG") == 0))
-    {
-
-        I_complete = (int *)calloc(nz, sizeof(int));
-        J_complete = (int *)calloc(nz, sizeof(int));
-        V_complete = (float *)calloc(nz, sizeof(float));
-
-        for (i = 0; i < nz; i++)
-        {
-            if (matcode[2] == 'P') {
-                fscanf(f, "%d %d", &I_complete[i], &J_complete[i]);
-                V_complete[i] = 1;
-            }  
-            else {
-                fscanf(f, "%d %d %f", &I_complete[i], &J_complete[i], &V_complete[i]);
-            } 
-            fscanf(f, "%*[^\n]\n");
-            /* adjust from 1-based to 0-based */
-            I_complete[i]--;
-            J_complete[i]--;
-        }
-    }
-
-    /* If the matrix is symmetric, we need to construct the other half */
-
-    else if ((strcmp(matcode, "MCRS") == 0) || (strcmp(matcode, "MCIS") == 0) || (strcmp(matcode, "MCPS") == 0) || (strcmp(matcode, "MCCS") == 0) || (strcmp(matcode, "MCCH") == 0) || (strcmp(matcode, "MCRK") == 0) || (matcode[0] == 'M' && matcode[1] == 'C' && matcode[2] == 'P' && matcode[3] == 'S'))
-    {
-
-        I_complete = (int *)calloc(2 * nz, sizeof(int));
-        J_complete = (int *)calloc(2 * nz, sizeof(int));
-        V_complete = (float *)calloc(2 * nz, sizeof(float));
-
-        int i_index = 0;
-
-        for (i = 0; i < nz; i++)
-        {
-            if (matcode[2] == 'P') {
-                fscanf(f, "%d %d", &I_complete[i], &J_complete[i]);
-                V_complete[i] = 1;
-            }
-            else {
-                fscanf(f, "%d %d %f", &I_complete[i], &J_complete[i], &V_complete[i]);
-            }
-                
-            fscanf(f, "%*[^\n]\n");
-
-            if (I_complete[i] == J_complete[i])
-            {
-                /* adjust from 1-based to 0-based */
-                I_complete[i]--;
-                J_complete[i]--;
-            }
-            else
-            {
-                /* adjust from 1-based to 0-based */
-                I_complete[i]--;
-                J_complete[i]--;
-                J_complete[nz + i_index] = I_complete[i];
-                I_complete[nz + i_index] = J_complete[i];
-                V_complete[nz + i_index] = V_complete[i];
-                i_index++;
-            }
-        }
-        nz += i_index;
-    }
-    else
-    {
-        printf("This matrix type is not supported: %s \n", matcode);
-        exit(1);
-    }
-
-    /* sort COO array by the rows */
-    if (!isSorted(J_complete, I_complete, nz)) {
-        quicksort(J_complete, I_complete, V_complete, nz);
-    }
-    
-    /* Convert from COO to CSR */
-    int *rowPtr = (int *)calloc(A_num_rows + 1, sizeof(int));
-    int *colIndex = (int *)calloc(nz, sizeof(int));
-    float *values = (float *)calloc(nz, sizeof(float));
-
-    for (i = 0; i < nz; i++) {
-        colIndex[i] = J_complete[i];
-        values[i] = V_complete[i];
-        rowPtr[I_complete[i] + 1]++;
-    }
-    for (i = 0; i < A_num_rows; i++) {
-        rowPtr[i + 1] += rowPtr[i];
-    }
-    A_nnz = nz;
-    /*******************************************************************/
 
     return 0;
 }
