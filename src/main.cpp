96b621de35f9a2d9ce99a21a4e36f18f09293acc
shanti <santiago.quintas@hotmail.com>
2026-01-04 23:29:28 +0000

    Add conditions for binary and complex matrices

--- src/main.cpp
+++ src/main.cpp
@@ -17,26 +17,58 @@ struct COO {
     double val;
 };
 
-void readMtxCoordinates(FILE* f, std::vector<COO>& entries, const int &nz){
+COO readMtxLine(FILE* f, MM_typecode matcode){
     int row, col;
     double val;
-    for (int i = 0; i < nz; i++) {
-        if (fscanf(f, "%d %d %lf", &row, &col, &val) != 3) {
-            std::cerr << "Error reading MTX entry at index " << i << "\n";
+    if (mm_is_pattern(matcode)) {
+        if (fscanf(f, "%d %d", &row, &col) != 2) {
+            std::cerr << "Error reading MTX entry at index\n";
             exit(1);
         }
+        val = 1;
+    } 
+    else if (mm_is_complex(matcode)) {
+        // TO BE IMPLEMENTED
+    }
+    else {
+        if (fscanf(f, "%d %d %lf", &row, &col, &val) != 3) {
+            std::cerr << "Error reading MTX entry at index\n";
+            exit(1);
+        }
+    }
+    return COO{row - 1, col - 1, val};
+}
 
-        // Convert from 1-based to 0-based indexing
-        entries.push_back({row - 1, col - 1, val});
+void readMtxCoordinates(FILE* f, MM_typecode matcode, std::vector<COO>& entries, int nz){
+    for (int i = 0; i < nz; i++) {
+        COO coordinate = readMtxLine(f, matcode);
+        entries.push_back(coordinate);
+
+        if (mm_is_symmetric(matcode) && coordinate.row != coordinate.col){
+            entries.push_back({coordinate.col, coordinate.row, coordinate.val});
+        }
+    }
+}
+
+void printCOO(const std::vector<COO>& entries)
+{
+    for (size_t i = 0; i < entries.size(); ++i) {
+        std::cout << "Entry " << i << ": "
+                  << "row = " << entries[i].row
+                  << ", col = " << entries[i].col
+                  << ", val = " << entries[i].val
+                  << "\n";
     }
 }
 
 int main(int argc, char* argv[]) {
 
     // TODO:
-    // create structure to store matrix information
     // properly read matrix information into structure
     // fill-in missing information
+    // if not matrix and not sparse, give error
+    // confirm if its being properly printed
+    // implement reading of complex values
 
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <mtx file>\n";
@@ -68,12 +100,12 @@ int main(int argc, char* argv[]) {
     }
 
     std::vector<COO> entries;
-    readMtxCoordinates(f, entries, mtx.nz);
-    
+    readMtxCoordinates(f, matcode, entries, mtx.nz);
 
     std::cout << "mtx rows: " << mtx.num_rows << "\n";
     std::cout << "mtx cols: " << mtx.num_cols << "\n";
     std::cout << "mtx nz: " << mtx.nz << "\n";
+    printCOO(entries);
 
     return 0;
 }
