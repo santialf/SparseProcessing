daa779b2aae7f5c9ea96d7a3e7204bfd957bbfbb
shanti <santiago.quintas@hotmail.com>
2026-01-07 23:47:14 +0000

    Add logic to convert coo to csr

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ COO readMtxLine(FILE* f, MM_typecode matcode)
     return COO{row - 1, col - 1, val};
 }
 
-void readMtxCoordinates(FILE* f, MM_typecode matcode, std::vector<COO>& entries, int nz)
+void readMtxCoordinates(FILE* f, MM_typecode matcode, std::vector<COO>& entries, const int nz)
 {
     for (int i = 0; i < nz; i++) {
         COO coordinate = readMtxLine(f, matcode);
@@ -86,9 +86,21 @@ void sortCOO(std::vector<COO>& entries)
         });
 }
 
-void convertCOOToCSR(const std::vector<COO>& entries, std::vector<CSR>& csr) 
+void convertCOOToCSR(const std::vector<COO>& coo, CSR& csr, const int nrows)
 {
+    csr.row_ptr.assign(nrows + 1, 0);
 
+    for (const auto& e : coo) 
+    {
+        csr.col_idx.push_back(e.col);
+        csr.values.push_back(e.val);
+        csr.row_ptr[e.row + 1]++;
+    }
+
+    for (int i = 0; i < nrows; i++)
+    {
+        csr.row_ptr[i + 1] += csr.row_ptr[i];
+    }
 }
 
 int main(int argc, char* argv[]) 
@@ -137,8 +149,8 @@ int main(int argc, char* argv[])
     readMtxCoordinates(f, matcode, entries, mtx.nz);
     sortCOO(entries);
 
-    std::vector<CSR> csr;
-    convertCOOToCSR(entries, csr);
+    CSR csr;
+    convertCOOToCSR(entries, csr, mtx.num_rows);
 
     std::cout << "mtx rows: " << mtx.num_rows << "\n";
     std::cout << "mtx cols: " << mtx.num_cols << "\n";
