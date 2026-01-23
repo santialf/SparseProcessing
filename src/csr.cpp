37950d014ba2200718f84a66f841bca0df70592d
shanti <santiago.quintas@hotmail.com>
2026-01-23 20:40:27 +0000

    Change CSR class to use arrays instead of vectors

--- src/csr.cpp
+++ src/csr.cpp
@@ -5,26 +5,27 @@ namespace mtx {
 template<typename valueType>
 void CSR<valueType>::print()
 {
-    std::cout << "row_ptr: ";
-    for (size_t v : row_ptr) 
+    std::cout << "rows: " << nrows_ << " " 
+              << "cols: " << ncols_ << " " 
+              << "nnzs: " << nnz_ << "\n";
+
+    std::cout << "rowPtr: ";
+    for (size_t i=0; i<nrows_ + 1; i++)
     {
-        std::cout << v << " ";
+        std::cout << rowPtr_[i] << " ";
     }
     std::cout << "\n";
-
-    std::cout << "col_idx: ";
-    for (size_t v : col_indices) 
+    std::cout << "colIdx: ";
+    for (size_t i=0; i<nnz_; i++)
     {
-        std::cout << v << " ";
-    }   
-    std::cout << "\n";
-
-    std::cout << "values : ";
-    for (valueType v : values) 
-    {
-        std::cout << v << " ";
+        std::cout << colIdx_[i] << " ";
     }
     std::cout << "\n";
+    std::cout << "vals: ";
+    for (size_t i=0; i<nnz_; i++)
+    {
+        std::cout << vals_[i] << " ";
+    }
 }
 
 } // namespace mtx
--- src/csr.hpp
+++ src/csr.hpp
@@ -8,20 +8,70 @@
 
 namespace mtx {
 
-template<typename valueType>
-class CSR : public sparseFormat {
+template<typename Value>
+class CSR {
 public:
-    std::vector<size_t> row_ptr;
-    std::vector<size_t> col_indices;
-    std::vector<valueType> values;
+    using deleter_t = void(*)(void*) noexcept;
 
-    CSR(size_t rows, size_t cols)
-    : sparseFormat(rows, cols) {}
+    // 1) Caller retains ownership of row/col/val
+    CSR(size_t* rowPtr, size_t* colIdx, Value* vals,
+        size_t nrows, size_t ncols, size_t nnz) noexcept
+        : rowPtr_(rowPtr), colIdx_(colIdx), vals_(vals),
+          nrows_(nrows), ncols_(ncols), nnz_(nnz)
+    {}
 
-    size_t getNnz() const override {
-        return values.size();
-    }
+    // 3) Adopt ownership of externally allocated buffers
+    struct adopt_t {};
+    static constexpr adopt_t adopt{};
+
+    CSR(adopt_t,
+        size_t* rowPtr, size_t* colIdx, Value* vals,
+        size_t nrows, size_t ncols, size_t nnz) noexcept
+        : rowPtr_(rowPtr), colIdx_(colIdx), vals_(vals),
+          nrows_(nrows), ncols_(ncols), nnz_(nnz),
+          rowPtr_owner_(rowPtr, csr_deleter),
+          colIdx_owner_(colIdx, csr_deleter),
+          vals_owner_(vals, csr_deleter)
+    {}
+
     void print();
+
+    size_t* rowPtr() noexcept { return rowPtr_; }
+    size_t* colIdx() noexcept { return colIdx_; }
+    Value* vals() noexcept { return vals_; }
+
+    const size_t* rowPtr() const noexcept { return rowPtr_; }
+    const size_t* colIdx() const noexcept { return colIdx_; }
+    const Value* vals() const noexcept { return vals_; }
+
+    size_t nrows() const noexcept { return nrows_; }
+    size_t ncols() const noexcept { return ncols_; }
+    size_t nnz()   const noexcept { return nnz_; }
+
+    bool owns_data() const noexcept { return rowPtr_owner_ != nullptr; }
+
+    CSR(const CSR&) = delete;
+    CSR& operator=(const CSR&) = delete;
+
+    CSR(CSR&&) = default;
+    CSR& operator=(CSR&&) = default;
+
+    ~CSR() = default;
+
+private:
+    static void csr_deleter(void* p) noexcept { std::free(p); }
+
+    size_t nrows_ = 0;
+    size_t ncols_ = 0;
+    size_t nnz_   = 0;
+
+    size_t* rowPtr_ = nullptr;
+    size_t* colIdx_ = nullptr;
+    Value* vals_ = nullptr;
+
+    std::unique_ptr<void, deleter_t> rowPtr_owner_{nullptr, nullptr};
+    std::unique_ptr<void, deleter_t> colIdx_owner_{nullptr, nullptr};
+    std::unique_ptr<void, deleter_t> vals_owner_{nullptr, nullptr};
 };
 
 } // namespace mtx
