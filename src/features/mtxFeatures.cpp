0af1867409d469bfa6095053814adf2d005f1a09
shanti <santiago.quintas@hotmail.com>
2026-02-05 20:21:18 +0000

    Add mean of nnzs per row feature

--- src/features/mtxFeatures.cpp
+++ src/features/mtxFeatures.cpp
@@ -21,6 +21,47 @@ double sparsity(const FormatType& mtx) {
   return 1 - density(mtx);
 }
 
+template <typename IndexType, typename ValueType>
+double nnzsPerRowMean(const CSR<IndexType, ValueType>& csr,
+                      const bool excludeEmptyRows) {
+  double rowMean = 0;
+  IndexType nonEmptyRows = 0;
+
+  for (IndexType i = 0; i < csr.nrows(); ++i) {
+    IndexType nnzs = csr.rowPtr()[i + 1] - csr.rowPtr()[i];
+    if (nnzs == 0) continue;
+    rowMean += nnzs;
+    nonEmptyRows++;
+  }
+  if (excludeEmptyRows) {
+    rowMean = rowMean / nonEmptyRows;
+  } else {
+    rowMean = rowMean / csr.nrows();
+  }
+
+  return rowMean;
+}
+
+/* template <typename IndexType, typename ValueType>
+double nnzsPerRowStandardDeviation(const CSR<IndexType, ValueType>& csr,
+                                   const bool excludeEmptyRows,
+                                   const double mean) {
+  if (mean == -1) mean = nnzsPerRowMean(csr, excludeEmptyRows);
+
+  double rowStandardDeviation = 0;
+  return rowStandardDeviation;
+} */
+
+/* template <typename IndexType, typename ValueType>
+double rowImbalanceFactor(const CSR<IndexType, ValueType>& csr,
+                          const bool excludeEmptyRows) {
+  auto rowMean = nnzsPerRowMean(csr, excludeEmptyRows);
+  auto rowStd = nnzsPerRowStandardDeviation(csr, excludeEmptyRows);
+  double imbalanceFactor =
+      static_cast<double>(rowStd) / static_cast<double>(rowMean);
+  return imbalanceFactor;
+} */
+
 // nnz_per_row_mean
 // nnz_per_row_std
 // nnz_per_col_mean
--- src/features/mtxFeatures.hpp
+++ src/features/mtxFeatures.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "formats/bell.hpp"
+#include "formats/coo.hpp"
+#include "formats/csc.hpp"
+#include "formats/csr.hpp"
+
 namespace mtx::features {
 
 namespace detail {
@@ -7,13 +12,24 @@ namespace detail {
 template <typename FormatType>
 double totalEntries(const FormatType&);
 
-}
+}  // namespace detail
 
 template <typename FormatType>
 double density(const FormatType&);
 
 template <typename FormatType>
 double sparsity(const FormatType&);
+
+template <typename IndexType, typename ValueType>
+double nnzsPerRowMean(const CSR<IndexType, ValueType>&, const bool = false);
+
+/* template <typename IndexType, typename ValueType>
+double nnzsPerRowStandardDeviation(const CSR<IndexType, ValueType>&,
+                                   const bool = false); */
+/*
+template <typename IndexType, typename ValueType>
+double rowImbalanceFactor(const CSR<IndexType, ValueType>&, const bool = false);
+*/
 
 }  // namespace mtx::features
 
