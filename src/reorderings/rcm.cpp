9746edf04b53b31f6c43c6180ffeaa6b23eef438
shanti <santiago.quintas@hotmail.com>
2026-03-15 17:25:13 +0000

    plan for rcm

--- src/reorderings/rcm.cpp
+++ src/reorderings/rcm.cpp
@@ -1,11 +1,27 @@
 #include "rcm.hpp"
 
+#include <queue>
+
 namespace mtx::reorderings {
 
 template <typename IndexType, typename ValueType>
+IndexType findPeripheralNodeId(const CSR<IndexType, ValueType>& csr,
+                               std::vector<IndexType> visited_nodes) {
+  return 0;
+}
+
+template <typename IndexType, typename ValueType>
 std::vector<IndexType> rcm(const CSR<IndexType, ValueType>& csr) {
-  std::cout << "hello world\n";
-  std::vector<IndexType> perm(0);
+  std::vector<IndexType> perm(csr.nrows()), visited_nodes;
+  std::queue<IndexType> visit_queue;
+
+  IndexType peripheral_node_id = findPeripheralNodeId(csr, visited_nodes);
+  // place peripheral node in the queue
+  // while loop iterating over the queue
+  // place neighboring nodes in the queue by order of minimum degree
+  // if queue is empty but there are still nodes to visit start over with
+  // finding peripheral node
+
   return perm;
 }
 }  // namespace mtx::reorderings
--- src/reorderings/rcm.hpp
+++ src/reorderings/rcm.hpp
@@ -8,8 +8,13 @@
 #include "formats/csr.hpp"
 
 namespace mtx::reorderings {
+
 template <typename IndexType, typename ValueType>
-std::vector<IndexType> rcm(const CSR<IndexType, ValueType>& csr);
+IndexType findPeripheralNodeId(const CSR<IndexType, ValueType>&,
+                               std::vector<IndexType>);
+
+template <typename IndexType, typename ValueType>
+std::vector<IndexType> rcm(const CSR<IndexType, ValueType>&);
 }  // namespace mtx::reorderings
 
 #ifdef _HEADER_ONLY
