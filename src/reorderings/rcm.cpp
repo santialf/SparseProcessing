bbe0f3443b4bcefa87d7cdc3fc0b1796215133cf
shanti <santiago.quintas@hotmail.com>
2026-04-25 13:25:42 +0100

    fix rcm reordering and update peripheral function

--- src/reorderings/rcm.cpp
+++ src/reorderings/rcm.cpp
@@ -1,5 +1,6 @@
 #include "rcm.hpp"
 
+#include <algorithm>
 #include <queue>
 #include <utility>
 #include <vector>
@@ -7,59 +8,43 @@
 namespace mtx::reorderings {
 
 template <typename IndexType, typename ValueType>
-IndexType getNodeDegree(const CSR<IndexType, ValueType>& csr,
-                        IndexType current_node) {
-  return csr.rowPtr()[current_node + 1] - csr.rowPtr()[current_node];
+IndexType getNodeDegree(const CSR<IndexType, ValueType>& csr, IndexType node) {
+  return csr.rowPtr()[node + 1] - csr.rowPtr()[node];
 }
 
+// Function to find the farthest node from a given root using BFS
 template <typename IndexType, typename ValueType>
-IndexType findPeripheralNodeId(const CSR<IndexType, ValueType>& csr,
-                               IndexType root) {
-  std::vector<bool> visited_nodes(csr.nrows(), false);
-  std::queue<std::pair<IndexType, IndexType>> bfs_queue;
-  IndexType number_of_levels = 1, max_number_of_levels = 0, local_root;
-  bfs_queue.push({root, 0});
+IndexType findFarthestNode(const CSR<IndexType, ValueType>& csr,
+                           IndexType root) {
+  std::vector<char> visited(csr.nrows(), 0);
+  std::queue<IndexType> bfs_queue;
+  bfs_queue.push(root);
+  visited[root] = 1;
+  IndexType farthest = root;
 
-  while (number_of_levels > max_number_of_levels) {
-    // go through all of the nodes in a connected component
-    while (!bfs_queue.empty()) {
-      IndexType current_node = bfs_queue.front().first;
-      IndexType current_level = bfs_queue.front().second;
-      std::vector<IndexType> neighbors;
+  while (!bfs_queue.empty()) {
+    IndexType node = bfs_queue.front();
+    bfs_queue.pop();
+    farthest = node;
 
-      // visit neighbors of the current node
-      for (IndexType i = csr.rowPtr()[current_node];
-           i < csr.rowPtr()[current_node + 1]; i++) {
-        IndexType neighborId = csr.colIdx()[i];
-
-        if (!visited_nodes[neighborId]) {
-          neighbors.push_back(neighborId);
-          visited_nodes[neighborId] = true;
-        }
+    for (IndexType i = csr.rowPtr()[node]; i < csr.rowPtr()[node + 1]; ++i) {
+      IndexType neighbor = csr.colIdx()[i];
+      if (!visited[neighbor]) {
+        visited[neighbor] = 1;
+        bfs_queue.push(neighbor);
       }
-
-      // sort neighbors by degree
-      std::sort(neighbors.begin(), neighbors.end(),
-                [&](IndexType a, IndexType b) {
-                  return getNodeDegree(csr, a) > getNodeDegree(csr, b);
-                });
-
-      // push neighbors to queue
-      for (IndexType neighbor : neighbors) {
-        bfs_queue.push({neighbor, current_level + 1});
-      }
-
-      bfs_queue.pop();
-      number_of_levels = current_level;
-      local_root = current_node;
-    }
-
-    if (number_of_levels >= max_number_of_levels) {
-      max_number_of_levels = number_of_levels;
-      root = local_root;
     }
   }
-  return root;
+  return farthest;
+}
+
+// Function to find a peripheral node by finding the farthest node from the
+// farthest node
+template <typename IndexType, typename ValueType>
+IndexType findPeripheralNode(const CSR<IndexType, ValueType>& csr,
+                             IndexType start) {
+  IndexType farthest = findFarthestNode(csr, start);
+  return findFarthestNode(csr, farthest);
 }
 
 template <typename IndexType, typename ValueType>
@@ -71,8 +56,9 @@ std::vector<IndexType> rcm(const CSR<IndexType, ValueType>& csr) {
 
   for (IndexType i = 0; i < csr.nrows(); i++) {
     if (!visited_nodes[i]) {
-      IndexType peripheral_node_id = findPeripheralNodeId(csr, i);
+      IndexType peripheral_node_id = findPeripheralNode(csr, i);
       visit_queue.push(peripheral_node_id);
+      visited_nodes[peripheral_node_id] = true;
 
       // go through all of the nodes in a connected component
       while (!visit_queue.empty()) {
@@ -80,9 +66,9 @@ std::vector<IndexType> rcm(const CSR<IndexType, ValueType>& csr) {
         std::vector<IndexType> neighbors;
 
         // visit neighbors of the current node
-        for (IndexType i = csr.rowPtr()[current_node];
-             i < csr.rowPtr()[current_node + 1]; i++) {
-          IndexType neighborId = csr.colIdx()[i];
+        for (IndexType j = csr.rowPtr()[current_node];
+             j < csr.rowPtr()[current_node + 1]; j++) {
+          IndexType neighborId = csr.colIdx()[j];
 
           if (!visited_nodes[neighborId]) {
             neighbors.push_back(neighborId);
@@ -109,6 +95,7 @@ std::vector<IndexType> rcm(const CSR<IndexType, ValueType>& csr) {
     }
   }
 
+  std::reverse(perm.begin(), perm.end());
   return perm;
 }
 }  // namespace mtx::reorderings
